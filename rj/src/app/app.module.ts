d9f7e896048798ab1fc41d5bc45655ebb8552ae2
janumpalli <jrahul119@gmail.com>
2018-12-01 02:09:41 -0500

    second commit

--- rj/src/app/app.module.ts
+++ rj/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Rahul1Component } from './rahul1/rahul1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Rahul1Component
   ],
   imports: [
     BrowserModule,
---
+++ rj/src/app/rahul1/rahul1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rahul1',
+  templateUrl: './rahul1.component.html',
+  styleUrls: ['./rahul1.component.sass']
+})
+export class Rahul1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
